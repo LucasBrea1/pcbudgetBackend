5caec105657606713c7d32837d3700cc3b138c5c
LucasBrea1 <150041633+LucasBrea1@users.noreply.github.com>
2024-10-31 22:53:53 -0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const usercontroller = require('../controllers/usersController')
 
-router.get('/', usercontroller.getUsers)
+
 router.post('/login', usercontroller.login)
 router.post('/register', usercontroller.register)
 
